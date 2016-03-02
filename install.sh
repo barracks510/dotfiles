67e19b7c2c29f63105748b701fff14f66e66d72f
Dennis Chen <barracks510@gmail.com>
2016-03-01 23:10:02 -0500

    Update Script and settings.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ source requirements.sh
 
 DEPS=""
 function read_deps() {
+	echo "Installing dependencies"
 	for dep in "${deps[@]}"; do
 		DEPS+="$dep "
 	done
@@ -13,20 +14,21 @@ if [ "$EUID" -eq 0 ]; then
 	echo "This script should not be run as root." 1>&2
 	exit 1
 fi
-if [ "$PWD" -eq "$HOME/.config/dotfiles" ]; then
+if [ "$PWD" != "$HOME/.config/dotfiles" ]; then
 	echo "You have installed this in the wrong directory." 1>&2
 	echo "Please reinstall into $HOME/.config/dotfiles." 1>&2
 	exit 1
 fi
 
 if [ -e /etc/fedora-release ]; then
-	FEDORA=$(cut /etc/fedora-release -d \' \' -f 3)
-	if [ "$FEDORA" -ne "23" ]; then
+	FEDORA=$(cut /etc/fedora-release -d \  -f 3)
+	if [ "$FEDORA" != "23" ]; then
 		echo "Fedora $FEDORA is not suppored. " 1>&2
 		echo "Installing anyways ..." 1>&2
 	fi
-	read_deps()
+	read_deps
 	sudo dnf install --assumeyes $DEPS
+	if [ -e $HOME/.tmux.conf ]
 	ln -s $HOME/.config/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
 	ln -s $HOME/.config/dotfiles/vim $HOME/.vim
 	ln -s $HOME/.vim/vimrc $HOME/.vimrc
---
+++ requirements.sh
@@ -0,0 +1,14 @@
+deps=(
+'vim-enhanced'
+'tmux'
+
+'vim-fugitive'
+'vim-latex'
+'vim-jedi'
+'vim-nerdtree'
+'vim-go'
+
+'powerline'
+'vim-powerline'
+'tmux-powerline'
+)
